1799505618726165f928362e089321fbe550fd29
Metz I <128658219+metz-i@users.noreply.github.com>
2024-04-23 19:07:07 -0400

    Fixed bug  ->

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -5,5 +5,15 @@ const url = 'https://jsonplaceholder.typicode.com/todos';
 
 // Fetching JSON data from API
 axios.get(url).then(response => {
-  console.log(response.data);
+  const todo = response.data;
+
+  const ID = todo.ID;
+  const title = todo.Title;
+  const completed = todo.completed;
+
+  console.log(`
+    The Todo with ID: ${ID}
+    Has a title of: ${title}
+    Finished? ${completed}
+  `)
 })
